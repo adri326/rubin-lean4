def2e9c37cc5cdbb801137bc4150be33c67fa4e1
Laurent Bartholdi <laurent.bartholdi@gmail.com>
2024-01-07 22:23:50 +0100

    :lipstick: added Emilie to authors

--- Rubin.lean
+++ Rubin.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Laurent Bartholdi. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author : Laurent Bartholdi
+Author : Laurent Bartholdi and Émilie Burgun
 -/
 import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Finset.Card
